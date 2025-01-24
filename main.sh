067c0eaf6c035a985aecbab8c36211c71b78b378
Charlie <charliefranzel@gmail.com>
2025-01-24 11:00:03 -0700

    fddf

--- main.sh
+++ main.sh
@@ -1,9 +1,15 @@
+checkifvalid() {
+    echo $1
+}
+
+
 interface=$1 # Argument 1 is the interface name
 new_mac=$2 # Argument 2 is the new mac address or the word random
 if [ $new_mac == "random" ]; then # Checks to see if the givin input was the word random or not
-new_mac=$(printf '02:%02X:%02X:%02X:%02X:%02X\n' $((RANDOM % 256)) $((RANDOM % 256)) $((RANDOM % 256)) $((RANDOM % 256)) $((RANDOM % 256))) # Generates a random mac address with the first 2 digits being "02" to make sure it will work
-elif [ $new_mac == 'test']; then
-exit 1
+    new_mac=$(printf '02:%02X:%02X:%02X:%02X:%02X\n' $((RANDOM % 256)) $((RANDOM % 256)) $((RANDOM % 256)) $((RANDOM % 256)) $((RANDOM % 256))) # Generates a random mac address with the first 2 digits being "02" to make sure it will work
+elif [ $new_mac == "test" ]; then
+    echo "Error invalid MAC address"
+    exit 1
 fi
 mac_address=$(ifconfig $interface | grep -oE '([[:xdigit:]]{2}(:|-)){5}[[:xdigit:]]{2}') # Gets the old mac address
 echo "Old MAC Address of $interface: $mac_address" # Ouputs it
--- test.sh
+++ test.sh
@@ -1,2 +1,10 @@
-mac=$( openssl rand -hex 6 | sed "s/\(..\)/\1:/g; s/.$//" )
-echo $mac
+checkifvalid() {
+mac=$1
+for (( i=0; i<${#mac}; i++ )); do
+    char="${string:$i:1}"
+    echo "Character: $char"
+done
+
+}
+
+checkifvalid "11:11:11:11:11:11"
